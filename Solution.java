5ab76b620910d4c323c77c53b05f86ed7ae9d6f8
SOURAV RAY <souravray288@gmail.com>
2025-12-12 22:34:42 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,18 @@
+// Transpose of Matrix
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        int n = mat.length;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(mat[j][i]);  // Swap row & column
+            }
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
